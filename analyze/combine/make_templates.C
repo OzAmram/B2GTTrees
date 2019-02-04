5fefa4f48eb684a99cbde11553cdd7edb320b162
Oz Amram <oamram1@jhu.edu>
2019-02-04 15:44:41 -0600

    Trying to get parametric hists to work for elel samesign fits

--- analyze/combine/make_templates.C
+++ analyze/combine/make_templates.C
@@ -57,9 +57,11 @@ TH1F* convert2d(TH2F *h_2d){
     for(int i=1; i<=n_xf_bins; i++){
         for(int j=1; j<=n_cost_bins; j++){
             float content = h_2d->GetBinContent(i,j);
+            float error = h_2d->GetBinError(i,j);
             int gbin = (i-1)*n_cost_bins + j;
             //printf("gbin %i: i j %i %i \n", gbin, i, j);
             h_1d->SetBinContent(gbin, content);
+            h_1d->SetBinError(gbin, error);
         }
     }
     return h_1d;
